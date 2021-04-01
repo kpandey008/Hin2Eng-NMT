2f069ca361f82c8061413d84eefa96cc11670439
Kushagra Pandey <kpandeyce008@gmail.com>
2021-04-01 19:59:25 +0530

    Updated tokenizer

--- nmt/train_tokenizer.py
+++ nmt/train_tokenizer.py
@@ -1,50 +1,19 @@
-from tokenizers import Tokenizer
-from tokenizers.models import BPE
-from tokenizers.pre_tokenizers import Whitespace
-from tokenizers.processors import TemplateProcessing
-from tokenizers.trainers import BpeTrainer
+import os
+
+from tokenizers import BertWordPieceTokenizer
 
 
-def train_de_tokenizer(files, vocab_size=30522, save_path=None):
-    de_tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
-    de_tokenizer.pre_tokenizer = Whitespace()
-    de_trainer = BpeTrainer(vocab_size=vocab_size, special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
-    de_tokenizer.train(files, de_trainer)
-    de_tokenizer.post_processor = TemplateProcessing(
-        single="[CLS] $A [SEP]",
-        pair="[CLS] $A [SEP] $B:1 [SEP]:1",
-        special_tokens=[
-            ("[CLS]", de_tokenizer.token_to_id("[CLS]")),
-            ("[SEP]", de_tokenizer.token_to_id("[SEP]")),
-        ],
-    )
-    print(f'Trained Hindi BPE tokenizer')
+def train_tokenizer(files, vocab_size=30522, save_path=None):
+    tokenizer = BertWordPieceTokenizer()
+    tokenizer.train(files, vocab_size=vocab_size, special_tokens = ["[S]","[PAD]","[/S]","[UNK]","[MASK]", "[SEP]","[CLS]"])
+    print(f'Trained tokenizer')
 
     if save_path is not None:
-        de_tokenizer.save(save_path)
-
-
-def train_en_tokenizer(files, vocab_size=30522, save_path=None):
-    en_tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
-    en_tokenizer.pre_tokenizer = Whitespace()
-    en_trainer = BpeTrainer(vocab_size=vocab_size, special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
-    en_tokenizer.train(files, en_trainer)
-    en_tokenizer.post_processor = TemplateProcessing(
-        single="[CLS] $A [SEP]",
-        pair="[CLS] $A [SEP] $B:1 [SEP]:1",
-        special_tokens=[
-            ("[CLS]", en_tokenizer.token_to_id("[CLS]")),
-            ("[SEP]", en_tokenizer.token_to_id("[SEP]")),
-        ],
-    )
-    print(f'Trained English BPE tokenizer')
-
-    if save_path is not None:
-        en_tokenizer.save(save_path)
+        os.makedirs(save_path, exist_ok=True)
+        tokenizer.save_model(save_path)
 
 
 if __name__ == '__main__':
     de_train = '/home/lexent/Hin2Eng-NMT/nmt/data/cleaned/train_hindi.csv'
     en_train = '/home/lexent/Hin2Eng-NMT/nmt/data/cleaned/train_english.csv'
-    train_de_tokenizer([de_train], save_path='/home/lexent/Hin2Eng-NMT/nmt/data/tokenizers/de_tokenizer.json')
-    train_en_tokenizer([en_train], save_path='/home/lexent/Hin2Eng-NMT/nmt/data/tokenizers/en_tokenizer.json')
+    train_tokenizer([de_train, en_train], save_path='/home/lexent/Hin2Eng-NMT/nmt/data/tokenizers')
