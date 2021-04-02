720fccf15593c945158ee6a6b09090f4adae0ccd
Kushagra Pandey <kpandeyce008@gmail.com>
2021-04-02 09:17:37 +0530

    Updated main training script

--- nmt/train.py
+++ nmt/train.py
@@ -2,10 +2,13 @@ import numpy as np
 import os
 import random
 import torch
+import torch.nn as nn
 import warnings
 
 from tokenizers import Tokenizer
-from transformers import EncoderDecoderModel
+from transformers import EncoderDecoderModel, BertConfig, EncoderDecoderConfig, BertForMaskedLM, BertTokenizerFast
+
+from dataset import Hin2EngDataset
 from trainer import TransformersForNmtTrainer
 
 
@@ -34,24 +37,38 @@ def seed_everything(seed=0):
     return seed
 
 
-# NOTE: Keeping the model as a param here as it will change a lot during experimentation
-# TODO: Complete this implementation after experimentation is done
 def train_nmt(
-    train_en_path, train_de_path, model,
-    en_tokenizer_path, de_tokenizer_path, n_epochs=100, code_size=10, device='cpu',
-    batch_size=32, random_state=0, save_path=os.getcwd(), trainer_kwargs={}
+    vocab_path, train_en_path, train_de_path, val_de_path=None, val_en_path=None,
+    n_epochs=10, device='cpu', batch_size=32, random_state=0, save_path=os.getcwd(),
+    **kwargs
 ):
+    # Seed
+    seed_everything(seed=random_state)
+
     # Load tokenizers
-    de_tokenizer = Tokenizer.from_file("de_tokenizer_path")
-    en_tokenizer = Tokenizer.from_file("en_tokenizer_path")
+    tokenizer = BertTokenizerFast(vocab_path)
 
-    # Dataset
-    train_dataset = Hin2EngDataset(train_de_path, train_en_path, de_tokenizer, en_tokenizer)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, collate_fn=train_dataset.collate_fn)
+    # Dataset and loaders
+    train_dataset = Hin2EngDataset(train_de_path, train_en_path, tokenizer, max_length=512)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, collate_fn=train_dataset.collate_fn, drop_last=True, shuffle=True)
 
-    # Train Loss
-    train_loss = nn.CrossEntropyLoss(ignore_index=en_tokenizer.token_to_id('[PAD]'))
+    val_loader = None
+    if (val_de_path is not None) and (val_en_path is not None):
+        val_dataset = Hin2EngDataset(val_de_path, val_en_path, tokenizer, max_length=512)
+        val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, collate_fn=val_dataset.collate_fn, drop_last=False, shuffle=False)
+
+    # Model
+    model = EncoderDecoderModel.from_encoder_decoder_pretrained('google/bert_uncased_L-8_H-512_A-8', 'google/bert_uncased_L-8_H-512_A-8')
 
     # Trainer
-    trainer = NMTTrainer(train_loader, model, train_loss, backend=device, random_state=random_state)
-    trainer.train(n_metric_epochs, save_path)
+    n_epochs = 10
+    trainer = TransformersForNmtTrainer(
+        train_loader,
+        model,
+        n_epochs,
+        val_loader=val_loader,
+        backend=device,
+        results_dir=save_path,
+        **kwargs
+    )
+    trainer.train()
